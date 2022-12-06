fdf745d551fbb8737545f17e35a1b35c60d162c8
xzylonite <xzylonite@gmail.com>
2022-12-07 01:17:00 +0900

    s3 uploads

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,6 +1,6 @@
-import multer from "multer";
-import multerS3 from "multer-s3";
-import aws from "aws-sdk";
+import multer from 'multer';
+import multerS3 from 'multer-s3';
+import aws from 'aws-sdk';
 
 const s3 = new aws.S3({
   credentials: {
@@ -9,23 +9,23 @@ const s3 = new aws.S3({
   },
 });
 
-const isHeroku = process.env.NODE_ENV === "production";
+const isHeroku = process.env.NODE_ENV === 'production';
 
 const s3ImageUploader = multerS3({
   s3: s3,
-  bucket: "wetubeee/images",
-  acl: "public-read",
+  bucket: process.env.AWS_BUCKET_NAME + '/images',
+  acl: 'public-read',
 });
 
 const s3VideoUploader = multerS3({
   s3: s3,
-  bucket: "wetubeee/videos",
-  acl: "public-read",
+  bucket: process.env.AWS_BUCKET_NAME + '/videos',
+  acl: 'public-read',
 });
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
-  res.locals.siteName = "Wetube";
+  res.locals.siteName = 'Wetube';
   res.locals.loggedInUser = req.session.user || {};
   res.locals.isHeroku = isHeroku;
   next();
@@ -35,8 +35,8 @@ export const protectorMiddleware = (req, res, next) => {
   if (req.session.loggedIn) {
     return next();
   } else {
-    req.flash("error", "Log in first.");
-    return res.redirect("/login");
+    req.flash('error', 'Log in first.');
+    return res.redirect('/login');
   }
 };
 
@@ -44,13 +44,13 @@ export const publicOnlyMiddleware = (req, res, next) => {
   if (!req.session.loggedIn) {
     return next();
   } else {
-    req.flash("error", "Not authorized");
-    return res.redirect("/");
+    req.flash('error', 'Not authorized');
+    return res.redirect('/');
   }
 };
 
 export const avatarUpload = multer({
-  dest: "uploads/avatars/",
+  dest: 'uploads/avatars/',
   limits: {
     fileSize: 3000000,
   },
@@ -58,7 +58,7 @@ export const avatarUpload = multer({
 });
 
 export const videoUpload = multer({
-  dest: "uploads/videos/",
+  dest: 'uploads/videos/',
   limits: {
     fileSize: 10000000,
   },
